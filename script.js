9bbceff613b2a4530432c58f10346acd7cd4cd76
Garming <garming_sam@outlook.com>
2014-05-05 01:29:54 +1200

    Add initial d3.js stuff

---
+++ script.js
@@ -0,0 +1,18 @@
+var w=960,h=500,
+svg=d3.select("#chart")
+.append("svg")
+.attr("width",w)
+.attr("height",h);
+ 
+var text=svg
+.append("text")
+.text("hello world")
+.attr("y",50);
+
+d3.csv('2008-Table1.csv', function(e){
+	console.log(e);
+	for (var i = 0; i < e.length; i++) {
+		console.log(e[i].Score);
+	}
+})
+
