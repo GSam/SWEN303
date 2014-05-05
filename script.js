d06059835aadb80a2ca1ee8f37a5be2d3dcde7c0
GS <garming_sam@outlook.com>
2014-05-06 09:38:42 +1200

    add more csv files. fix css

--- script.js
+++ script.js
@@ -14,5 +14,18 @@ d3.csv('2008-Table1.csv', function(e){
 	for (var i = 0; i < e.length; i++) {
 		console.log(e[i].Score);
 	}
-})
+	d3.csv('2009-Table1.csv', function(e){
+		d3.csv('2010-Table1.csv', function(e){
+			d3.csv('2011-Table1.csv', function(e){
+				d3.csv('2012-Table1.csv', function(e){
 
+
+
+				});
+
+			});
+
+		});
+
+	});
+});
