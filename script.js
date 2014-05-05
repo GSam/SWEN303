be86ea5c4c2106221273c6a99a02dfbf108335b6
GS <garming_sam@outlook.com>
2014-05-06 09:43:43 +1200

    Store in overall object, recording years

--- script.js
+++ script.js
@@ -9,16 +9,21 @@ var text=svg
 .text("hello world")
 .attr("y",50);
 
+var allGames = {};
 d3.csv('2008-Table1.csv', function(e){
 	console.log(e);
 	for (var i = 0; i < e.length; i++) {
 		console.log(e[i].Score);
 	}
+	allGames['2008'] = e;
 	d3.csv('2009-Table1.csv', function(e){
+		allGames['2009'] = e;
 		d3.csv('2010-Table1.csv', function(e){
+			allGames['2010'] = e;
 			d3.csv('2011-Table1.csv', function(e){
+				allGames['2011'] = e;
 				d3.csv('2012-Table1.csv', function(e){
-
+					allGames['2012'] = e;
 
 
 				});
