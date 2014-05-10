a0b1d61bcb3a92a0385bc451debb0e1a4bf6b76b
Garming <garming_sam@outlook.com>
2014-05-10 17:30:21 +1200

    Added python script to cleanup csv files

---
+++ commands.py
@@ -0,0 +1,85 @@
+tables = ["2008-Table1.csv",
+          "2009-Table1.csv",
+          "2010-Table1.csv",
+          "2011-Table1.csv",
+          "2012-Table1.csv",
+          "2013-Table1.csv",]
+for table in tables:
+    f = open(table, 'r')
+    text = f.read()
+    f.close()
+    text = text.replace("Rotorua*", "Rotorua")
+    f = open(table, 'w')
+    f.write(text)
+    f.close()
+
+print "DONE ROTORUA"
+
+for table in tables:
+    f = open(table, 'r')
+    text = f.read()
+    f.close()
+    text = text.replace("Mystery Creek Events Centre,Hamilton", "Mystery Creek Events Centre, Hamilton")
+    f = open(table, 'w')
+    f.write(text)
+    f.close()
+
+print "DONE MYSTERY CREEK"
+
+for table in tables:
+    f = open(table, 'r')
+    text = f.read()
+    f.close()
+    text = text.replace("Brisbane Convention & Exhibition Centre", "Brisbane Convention and Exhibition Centre")
+    f = open(table, 'w')
+    f.write(text)
+    f.close()
+
+print "DONE BRISBANE CONVENTION"
+
+for table in tables:
+    f = open(table, 'r')
+    text = f.read()
+    f.close()
+    text = text.replace("State Netball and Hockey Centre,Melbourne", "State Netball and Hockey Centre, Melbourne")
+    f = open(table, 'w')
+    f.write(text)
+    f.close()
+
+print "DONE STATE AND HOCKEY"
+
+for table in tables:
+    f = open(table, 'r')
+    text = f.read()
+    f.close()
+    text = text.replace("Stadium Southland Velodrome,Invercargill", "Invercargill ILT Velodrome")
+    f = open(table, 'w')
+    f.write(text)
+    f.close()
+
+print "DONE VELODROME"
+
+for table in tables:
+    f = open(table, 'r')
+    text = f.read()
+    f.close()
+    text = text.replace("Te Rauparaha, Porirua", "Te Rauparaha Arena, Porirua")
+    f = open(table, 'w')
+    f.write(text)
+    f.close()
+
+print "DONE PORIRUA"
+
+for table in tables:
+    f = open(table, 'r')
+    text = f.read()
+    f.close()
+    text = text.replace("Westpac Centre, Christchurch", "Westpac Arena, Christchurch")
+    f = open(table, 'w')
+    f.write(text)
+    f.close()
+
+print "DONE WESTPAC"
+
+
+
