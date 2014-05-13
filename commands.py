a4a3897f6df11f3fbb76abc5ab89821822d8fc6d
Garming <garming_sam@outlook.com>
2014-05-13 18:58:55 +1200

    commands.py update

--- commands.py
+++ commands.py
@@ -107,10 +107,31 @@ for table in tables:
     f = open(table, 'r')
     text = f.read()
     f.close()
-    text = text.replace("Adelaide Entertainment Centre", "Adelaide Entertainment Centre, Adelaide")
+    text = text.replace(",Adelaide Entertainment Centre", ",\"Adelaide Entertainment Centre, Adelaide\"")
     f = open(table, 'w')
     f.write(text)
     f.close()
 
 print "DONE ADEL"
 
+for table in tables:
+    f = open(table, 'r')
+    text = f.read()
+    f.close()
+    text = text.replace("State Netball Hockey Centre, Melbourne", "State Netball and Hockey Centre, Melbourne")
+    f = open(table, 'w')
+    f.write(text)
+    f.close()
+
+print "DONE NETBALLAND"
+
+for table in tables:
+    f = open(table, 'r')
+    text = f.read()
+    f.close()
+    text = text.replace("Claudelands Arena, Waikato", "Claudelands Arena, Hamilton")
+    f = open(table, 'w')
+    f.write(text)
+    f.close()
+
+print "DONE CLAUDE"
