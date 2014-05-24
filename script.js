802bf28ead5ac82b86fa22bf101a3000289b818c
Garming <garming_sam@outlook.com>
2014-05-24 23:05:43 +1200

    Temporary

--- script.js
+++ script.js
@@ -172,6 +172,110 @@ function graph1() {
 	
 	});
 
+
+
+
+	var margin = {top: 10, right: 10, bottom: 100, left: 40},
+	margin2 = {top: 430, right: 10, bottom: 20, left: 40},
+	width = 960 - margin.left - margin.right,
+	height = 500 - margin.top - margin.bottom,
+	height2 = 500 - margin2.top - margin2.bottom;
+
+	var parseDate = d3.time.format("%b %Y").parse;
+
+	var x = d3.time.scale().range([0, width]),
+	x2 = d3.time.scale().range([0, width]),
+	y = d3.scale.linear().range([height, 0]),
+	y2 = d3.scale.linear().range([height2, 0]);
+
+	var xAxis = d3.svg.axis().scale(x).orient("bottom"),
+	xAxis2 = d3.svg.axis().scale(x2).orient("bottom"),
+	yAxis = d3.svg.axis().scale(y).orient("left");
+
+	var brush = d3.svg.brush()
+	.x(x2)
+	.on("brush", brushed);
+
+	var area = d3.svg.line()
+	.interpolate("monotone")
+	.x(function(d) { return x(d.date); })
+	//.y0(height)
+	.y(function(d) { return y(d.price); });
+
+	var area2 = d3.svg.line()
+	.interpolate("monotone")
+	.x(function(d) { return x2(d.date); })
+	//.y0(height2)
+	.y(function(d) { return y2(d.price); });
+
+	var svg = d3.select("body").append("svg")
+	.attr("width", width + margin.left + margin.right)
+	.attr("height", height + margin.top + margin.bottom);
+
+	svg.append("defs").append("clipPath")
+	.attr("id", "clip")
+	.append("rect")
+	.attr("width", width)
+	.attr("height", height);
+
+	var focus = svg.append("g")
+	.attr("class", "focus")
+	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	var context = svg.append("g")
+	.attr("class", "context")
+	.attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
+var data = [type({"date": "Jan 2000", "price":1000}),type({"date": "Jan 2005", "price":100}),type({"date": "Jan 2010", "price":500}) ];
+console.log(data);
+		x.domain(d3.extent(data.map(function(d) { return d.date; })));
+		y.domain([0, d3.max(data.map(function(d) { return d.price; }))]);
+		x2.domain(x.domain());
+		y2.domain(y.domain());
+
+		//focus.append('g').attr("clip-path", "url(#clip)").append("path")
+		focus.append('path')
+		.datum(data)
+		.attr("class", "area")
+		.attr("d", area).style('stroke', 'blue').style('stroke-width', '2').style('fill', 'none');
+
+		focus.append("g")
+		.attr("class", "x axis")
+		.attr("transform", "translate(0," + height + ")")
+		.call(xAxis);
+
+		focus.append("g")
+		.attr("class", "y axis")
+		.call(yAxis);
+
+		context.append("path")
+		.datum(data)
+		.attr("class", "area")
+		.attr("d", area2).style('stroke', 'blue').style('stroke-width', '2').style('fill', 'none');
+
+		context.append("g")
+		.attr("class", "x axis")
+		.attr("transform", "translate(0," + height2 + ")")
+		.call(xAxis2);
+
+		context.append("g")
+		.attr("class", "x brush")
+		.call(brush)
+		.selectAll("rect")
+		.attr("y", -6)
+		.attr("height", height2 + 7);
+
+	function brushed() {
+		x.domain(brush.empty() ? x2.domain() : brush.extent());
+		focus.select(".area").attr("d", area);
+		focus.select(".x.axis").call(xAxis);
+	}
+
+	function type(d) {
+		d.date = parseDate(d.date);
+		d.price = +d.price;
+		return d;
+	}
+
 }
  
 function isNumber(n){
@@ -660,7 +764,7 @@ d3.csv('2008-Table1.csv', function(e){
 
 							// create graph 1
 							graph1();
-							graph1();
+							//graph1();
 							d3.selectAll('svg').on('click', function(e){
 								d3.selectAll('svg').remove();
 								//switchTo('venue');
@@ -1086,4 +1190,5 @@ var r = [10 / 2, -10 / 2, projection.rotate()[2]]; s = projection.rotate(r);cons
 function readd(a) {
 	d3.select('#chart').append(function(){return a.node();})
 }
+
 
